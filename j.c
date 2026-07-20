54ab2aabb5053547ab3990549c6e3532964e40f8
Andrea Bonucci <89645554+BonucciAndrea@users.noreply.github.com>
2026-07-20 16:20:50 +0100

    Add files via upload

--- j.c
+++ j.c
@@ -1,4 +1,4 @@
-#include"a.h" // ngn/k, (c) 2019-2024 ngn, GNU AGPLv3 - https://codeberg.org/ngn/k/raw/branch/master/LICENSE
+#include"a.h" // Amber - GNU AGPLv3 - see LICENSE and NOTICE
 Z CO C je[]="\"\\/\b\f\n\r\t",ej[]="\"\\/bfnrt";Z C*s;
 
 Z A0 jx;Z C jw()_(W(*s&&*s<=32u,s++)*s)
--- k.c
+++ k.c
@@ -1,4 +1,4 @@
-#include"a.h" // ngn/k, (c) 2019-2024 ngn, GNU AGPLv3 - https://codeberg.org/ngn/k/raw/branch/master/LICENSE
+#include"a.h" // Amber - GNU AGPLv3 - see LICENSE and NOTICE
 #include"k.h"
 C TK(K x)_(Tk[xt])
 N NK(K x)_(xn)
